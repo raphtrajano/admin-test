48758a5e83df9fbde870e8574cc167afa39ab410
Raphael Trajano <raphaelsrt@gmail.com>
2020-10-12 23:34:30 -0300

    refactor: Add types folder to Profile component

---
+++ src/types/Profile.ts
@@ -0,0 +1,19 @@
+import { Record } from 'react-admin';
+
+export interface Customer extends Record {
+    first_name: string;
+    last_name: string;
+    address: string;
+    city: string;
+    zipcode: string;
+    avatar: string;
+    birthday: string;
+    first_seen: string;
+    last_seen: string;
+    has_ordered: boolean;
+    latest_purchase: string;
+    has_newsletter: boolean;
+    groups: string[];
+    nb_commands: number;
+    total_spent: number;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { Customer } from './Profile';
+
+export type {
+    Customer
+}
