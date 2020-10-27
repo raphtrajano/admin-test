a5f22d60db39e90bb3d004d2467df736cd437953
Raphael Trajano <raphaelsrt@gmail.com>
2020-10-27 00:22:24 -0200

    feat: Add filter to LudumHelp component

--- src/components/LudumHelp/LudumHelp.style.ts
+++ src/components/LudumHelp/LudumHelp.style.ts
@@ -4,11 +4,20 @@ const LudumHelpWrapper = styled.div`
     display: flex;
     width: 100%;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: end;
     .title {
-        margin: 0;
-        margin-top: 32px;
+        width: 100%;
+        margin: 32px 0;
         font-weight: bold;
+    }
+    .filter-box {
+        width: 200px;
+        border-radius: 10px;
+    }
+    .filter-content {
+        padding: 0 24px 16px 10px;
+        border: 2px solid rgba(16,69,116,1);
+        border-radius: 10px;
     }
 `;
 
--- src/components/LudumHelp/LudumHelp.tsx
+++ src/components/LudumHelp/LudumHelp.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
-import { List, Datagrid, NumberField, TextField } from 'react-admin';
-import { Typography } from "@material-ui/core";
+import { 
+  FilterList,
+  FilterListItem,
+  FilterLiveSearch,
+  useListContext, 
+  List,
+} from 'react-admin';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import HeadsetMicIcon from '@material-ui/icons/HeadsetMic';import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import { Typography, Card, CardContent } from "@material-ui/core";
 import Zendesk from "react-zendesk";
+import { ZendeskAPI } from "react-zendesk";
 
 import { LudumHelpWrapper } from './LudumHelp.style';
 
-const ZENDESK_KEY = '1HGwBOcHEAoHOIysC4Ls5txgCD4yRCVQXHBR77Ff';
+const ZENDESK_KEY = '049c6823-594c-4d1c-aafa-4d09c22ea5cd';
+ZendeskAPI("webWidget", "setLocale", "de");
 
 const setting = {
     color: {
@@ -23,17 +33,77 @@ const setting = {
     }
   };
 
+const Aside = () => {
+
+    return (
+        <Card className="filter-box">
+            <CardContent className="filter-content"> 
+                <FilterList
+                    label="Cases"
+                    icon={<LibraryBooksIcon />}
+                >
+                    <FilterListItem
+                        label="New"
+                        value={{
+                            sales_lte: undefined,
+                            sales_gt: 25,
+                            sales: undefined,
+                        }}
+                    />
+                    <FilterListItem
+                        label="Open"
+                        value={{
+                            sales_lte: 25,
+                            sales_gt: 10,
+                            sales: undefined,
+                        }}
+                    />
+                    <FilterListItem
+                        label="Closed"
+                        value={{
+                            sales_lte: 10,
+                            sales_gt: 0,
+                            sales: undefined,
+                        }}
+                    />
+                </FilterList>
+
+                <FilterList
+                    label="Contact Us"
+                    icon={<ContactMailIcon />}
+                >
+                </FilterList>
+
+                <FilterList
+                    label="Help Center"
+                    icon={<HeadsetMicIcon />}
+                >
+                </FilterList>
+            </CardContent>
+        </Card>
+    );
+};
+
 export const LudumHelp = props => {
     
     return (
-    <>
-        <Zendesk zendeskKey={ZENDESK_KEY} {...setting} onLoaded={() => console.log('is loaded')} />
-            <LudumHelpWrapper>
-                <Typography variant="h4" component="h1" align="center" className="title">
-                    Help Support
-                </Typography>
-            </LudumHelpWrapper>
-    </>
+    <LudumHelpWrapper>
+      <Zendesk zendeskKey={ZENDESK_KEY} {...setting} onLoaded={() => console.log('is loaded')} />
+      <Typography variant="h4" component="h1" align="center" className="title">
+        Help Support
+      </Typography>
+      <List 
+      {...props}
+      title=' '
+      sort={{ field: 'name', order: 'ASC' }}
+      perPage={20}
+      pagination={false}
+      component="div"
+      actions={false}
+      >
+        <Aside/>
+      </List>
+    </LudumHelpWrapper>
     );
 
 };
